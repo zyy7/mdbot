c4d216611ecf350f9340e7e283ce2ed5415a7dfe
zyy7 <jojoganz191107@gmail.com>
2022-04-01 23:31:49 +0700

    mdbot

---
+++ config.js
@@ -0,0 +1,73 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'sanzychan01',
+}
+
+// Other
+global.owner = ['6285924761802']
+
+global.namabot = 'Ex BotZ'
+
+global.namaowner = '𝙕𝙮𝙮𝙓𝙙 💀'
+
+global.myweb = 'https://www.instagram.com/jojo_wibu/'
+
+global.premium = ['6285924761802']
+
+global.packname = 'ᴍʏ ʙᴏᴛ ᴇx'
+
+global.author = 'WhatsApp Bot'
+
+global.sessionName = 'hisoka'
+
+global.prefa = ['','!','.','🐦','🐤','🗿']
+
+global.sp = '⭔'
+
+global.mess = {
+    success: '✓ Success',
+    
+    admin: 'Fitur Khusus Admin Group!',
+    
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    
+    owner: 'Fitur Khusus Owner Bot',
+    
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    
+    wait: 'Loading...',
+    
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/hisoka.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
